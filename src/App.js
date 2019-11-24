14f7948b2349ec2b08993d5ee18fde56092e9768
Gustavo <gugsliesenfeld@gmail.com>
2019-11-24 17:46:03 -0300

    feat(board): componente do tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.css';
-import King from './components/pieces/King';
-import Knight from './components/pieces/Knight';
-import Pawn from './components/pieces/Pawn';
-import Queen from './components/pieces/Queen';
-import Rook from './components/pieces/Rook';
-import Bishop from './components/pieces/Bishop';
-import Tile from './components/Tile';
+import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
-      <Tile alternate={true} piece={ <King></King> } />
-      <Tile piece={ <Knight></Knight> } />
-      <Tile alternate={true} piece={ <Pawn></Pawn> } />
-      <Tile piece={ <Queen></Queen> } />
-      <Tile alternate={true} piece={ <Rook></Rook> } />
-      <Tile piece={ <Bishop></Bishop> } />
+      <Board></Board>
     </div>
   ); 
 }
